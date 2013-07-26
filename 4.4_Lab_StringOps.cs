b45f4c55ce0e8f3158c357a35a7eb42ec1b410c8
Alethor <oscarbralo@outlook.com>
2013-07-26 09:16:52 +0200

    Update 4.4_Lab_StringOps.cs
    Fixed a couple of problems in case that you enter a empty string.
    I hope you like!

--- 4.4_Lab_StringOps.cs
+++ 4.4_Lab_StringOps.cs
@@ -1,55 +1,55 @@
 using System;
 
-class String_Operations
-{
-	static void read_input(string S)
-	{
-		int StrLen = S.Length;
-		int LastNum = StrLen - 1;
-		string LastStr = S.Substring(LastNum, 1);
-		
-		if(LastStr == ".") {
-			Console.WriteLine("You have entered a declarative sentence.");
-		}
-		else if(LastStr == "?") {
-			Console.WriteLine("You have entered an interrogatory sentence.");
-		}
-		else if(LastStr == "!") {
-			Console.WriteLine("You have entered an exclamatory sentence.");
-		}
-		else {
-			Console.WriteLine("You did not enter a sentence.");
-			}
-	} 
-	
-	static void name(string fullName)
+    class String_Operations
+    {
+        static void read_input(string S)
+        {
+            while (String.IsNullOrEmpty(S))
+            {
+                Console.Write("Enter a sentence with proper grammar: ");
+                S = Console.ReadLine();
+            }
+            int LastNum = S.Length - 1;
+            string LastStr = S[S.Length - 1].ToString();
+            switch (LastStr)
+            {
+                case ".":
+                    Console.WriteLine("You have entered a declarative sentence.");
+                    break;
+                case "!":
+                    Console.WriteLine("You have entered an exclamatory sentence");
+                    break;
+                case "?":
+                    Console.WriteLine("You have entered an interrogatory sentence.");
+                    break;
+                default:
+                    Console.WriteLine("You did not enter a sentence.");
+                    break;
+
+            }
+        }
+
+        static void name(string fullName)
 	{	
-		
-		int x = fullName.IndexOf(" ");
-		
-		if(x == -1) {
-			Console.WriteLine(fullName);
-		}
-	 	
-	 	else {
-	 	int l = fullName.Length;
-	 	int L = l - x;
-		
-		string lastName = fullName.Substring(x, L);
-		string firstName = fullName.Substring(0, x); 
-		
-		Console.WriteLine(lastName + ", " + firstName);
-		}
-		
+	    while(!fullName.Contains(" "))
+	    {
+	    	Console.Write("Enter your first and last name or a name you go by (a whitespace is needed): ");
+		fullName = Console.ReadLine();
+	    }
+            string[] name = fullName.Split(' ');
+
+            Console.WriteLine(name[1] + ", " + name[0]);
 	}
-	
-	static void Main()
-	{
-		Console.Write("Enter a sentence with proper grammar: ");
-		string S = Console.ReadLine();
-		read_input(S);
-		Console.Write("Enter your first and last name or a name you go by: ");
-		string fullName = Console.ReadLine();
-		name(fullName);	
-	}		
-}
+
+        static void Main()
+        {
+            Console.Write("Enter a sentence with proper grammar: ");
+            string S = Console.ReadLine();
+            read_input(S);
+            Console.Write("Enter your first and last name or a name you go by (a whitespace is needed): ");
+            string fullName = Console.ReadLine();
+            name(fullName);
+            Console.ReadLine();
+        }
+    }
+    
